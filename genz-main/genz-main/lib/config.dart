a7f93a66bbc3720e0d48067cabdc115108cf0384
ahmad <ahmadchakroun904@gmail.com>
2025-02-21 22:38:32 +0100

    aa1

--- genz-main/genz-main/lib/config.dart
+++ genz-main/genz-main/lib/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static const String appName = "Yosser App";
-  static const String apiURL = "10.0.2.2:4000";
+  static const String apiURL = "localhost:4000";
   static const String imageURL = "http://10.0.2.2:4000";
   static const String postAPI = "api/post";
   static const int pageSize = 10;
